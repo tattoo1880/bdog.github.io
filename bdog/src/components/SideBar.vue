9dc55c8899b8d596a3a30f2a49e621ae542aa545
tattoo1880 <tattoo186225345941@gmail.com>
2024-01-23 15:09:54 +0800

    update

--- bdog/src/components/SideBar.vue
+++ bdog/src/components/SideBar.vue
@@ -2,9 +2,9 @@
   <aside class="sidebar">
     <!-- 个人简介 -->
     <section class="profile">
-      <img src="../assets/joker.jpeg" alt="Your Name" width="200px" height="200px"
+      <img src="../assets/joker.jpeg" alt="Your Name" width="180px" height="180px"
         style="border-radius: 50%; margin-left:5px;margin-top: 2px;" />
-      <h3 style="margin-left: 50px;" class="atitle">Townly Stone</h3>
+      <h3 style="margin-left: 30px;" class="atitle">Townly Stone</h3>
       <p style="margin-left: 30px;" class="atitle">----------------</p>
     </section>
     <section class="my_info" style="margin-left: 50px;">
