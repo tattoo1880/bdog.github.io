5fbaac9a2434af742f4a262bbbc69abbf2f1853a
tattoo1880 <tattoo186225345941@gmail.com>
2024-01-23 11:38:13 +0800

    update

--- bdog/src/components/NewsC.vue
+++ bdog/src/components/NewsC.vue
@@ -14,7 +14,7 @@
             </el-card>
         </el-col>
     </el-row>
-    <el-dialog :visible.sync="showNew" width="30%" :before-close="handleClose">
+    <el-dialog :visible.sync="showNew" width="100%" >
         <span>{{ articleNew }}</span>
         <span slot="footer" class="dialog-footer">
             <el-button @click="showNew = false">取 消</el-button>
