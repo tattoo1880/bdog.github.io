94c59a0064824968dab56457d095a8b6c5520ed5
tattoo1880 <tattoo186225345941@gmail.com>
2024-01-23 12:43:13 +0800

    update

--- bdog/src/components/NewsC.vue
+++ bdog/src/components/NewsC.vue
@@ -38,18 +38,18 @@ const getNews = async () => {
             lang: 'zh-hans',
 
         },
-        // headers: {
-        //     'sec-ch-ua': '"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"',
-        //     'sec-ch-ua-mobile': '?0',
-        //     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
-        //     'Accept': 'application/json, text/javascript, */*; q=0.01',
-        //     'Cache-Control': 'max-age=600',
-        //     'Referer': 'https://cn.nytimes.com/',
-        //     'X-Requested-With': 'XMLHttpRequest',
-        //     'sec-ch-ua-platform': '"macOS"'
-        // }
+        headers: {
+            'sec-ch-ua': '"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"',
+            'sec-ch-ua-mobile': '?0',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+            'Accept': 'application/json, text/javascript, */*; q=0.01',
+            'Cache-Control': 'max-age=600',
+            'Referer': 'https://cn.nytimes.com/',
+            'X-Requested-With': 'XMLHttpRequest',
+            'sec-ch-ua-platform': '"macOS"'
+        }
     });
-    console.log(response.data.list.daily);
+    // console.log(response.data.list.daily);
     const l = response.data.list.daily
     let news = []
     // map l to news
