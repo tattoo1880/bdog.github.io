d017e9a7f86596bed215419a95ef2bbcf86814e3
tattoo1880 <tattoo186225345941@gmail.com>
2024-01-23 14:01:54 +0800

    update

--- bdog/src/utils/request.js
+++ bdog/src/utils/request.js
@@ -1,13 +1,13 @@
 import axios from 'axios'
 
 const service = axios.create({
-    baseURL: 'https://work.daweichuanmei.club/api',
+    baseURL: 'http://45.147.48.43/api',
     // baseURL: 'http://localhost:8000',
     timeout: 5000
 })
 
 const service2 = axios.create({
-    baseURL: 'https://work.daweichuanmei.club/apii',
+    baseURL: 'http://45.147.48.43/apii',
     timeout: 500000
 })
 
