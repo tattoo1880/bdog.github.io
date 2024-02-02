ed4376ca8a8d338444b8cfb7529615afa3e5b111
tattoo1880 <tattoo186225345941@gmail.com>
2024-02-02 17:25:26 +0800

    update

--- bdog/src/components/PornStarC.vue
+++ bdog/src/components/PornStarC.vue
@@ -8,7 +8,7 @@
         <el-switch v-model="value1" active-text="查看收藏" inactive-text="浏览" style="margin-left: 950px;" />
     </el-row>
     <el-row v-if="!value1">
-            <el-input v-model="search" placeholder="请输入明星名字" />
+            <el-input v-model="search" placeholder="请输入明星名字" style="width: 500px; margin-left: 200px;"/>
             <el-button type="primary" @click="searchStar">搜索</el-button>
     </el-row>
     <el-container  v-loading="loading" v-if="!value1">
