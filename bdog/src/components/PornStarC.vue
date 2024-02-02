e3438526a9e2fa439bdde22e7fbccfcb40c93df1
tattoo1880 <tattoo186225345941@gmail.com>
2024-02-02 17:27:54 +0800

    update

--- bdog/src/components/PornStarC.vue
+++ bdog/src/components/PornStarC.vue
@@ -8,12 +8,12 @@
         <el-switch v-model="value1" active-text="查看收藏" inactive-text="浏览" style="margin-left: 950px;" />
     </el-row>
     <el-row v-if="!value1">
-            <el-input v-model="search" placeholder="请输入明星名字" style="width: 500px; margin-left: 700px;"/>
+            <el-input v-model="search" placeholder="请输入明星名字" style="width: 300px; margin-left: 600px;"/>
             <el-button type="success" @click="searchStar" style="margin-left: 10px;" plain>搜索</el-button>
     </el-row>
     <el-container  v-loading="loading" v-if="!value1">
         <!-- 搜索栏，输入名字，从stardata中筛选出符合的对象 -->
-        <el-row v-if="!condition" style="margin-top: 40px;">
+        <el-row v-if="!condition" style="margin-top: 20px;">
             <el-col v-for="(item, index) in stardata" :key="index" :span="6" :offset="1">
                 <el-card :body-style="{ padding: '0px' }" class="card">
                     <img :src="item.img" class="image" />
