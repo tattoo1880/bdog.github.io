e8a859c0ec78952fb51267c8251b44dd3d9d8ec3
tattoo1880 <tattoo186225345941@gmail.com>
2024-02-02 16:40:28 +0800

    update

--- bdog/src/utils/request.js
+++ bdog/src/utils/request.js
@@ -18,8 +18,8 @@ const service3 = axios.create({
 })
 
 const service4 = axios.create({
-    baseURL: 'http://127.0.0.1:8080',
-    // baseURL: 'http://45.147.48.43:8080',
+    // baseURL: 'http://127.0.0.1:8080',
+    baseURL: 'http://45.147.48.43:8080',
     timeout: 50000,
 })
 
