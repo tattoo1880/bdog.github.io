b2c9daac822b8e764f9148ec133641c8d269d373
tattoo1880 <tattoo186225345941@gmail.com>
2024-02-26 13:21:09 +0800

    update

--- bdog/src/utils/request.js
+++ bdog/src/utils/request.js
@@ -28,5 +28,12 @@ const service5 = axios.create({
     baseURL: 'http://45.147.48.43:8080/channel',
     timeout: 50000,
 })
+
+const service6 = axios.create({
+    baseURL: 'http://127.0.0.1:8080/download',
+    // baseURL: 'http://45.147.48.43:8080/download',
+    timeout: 50000,
+})
+
 export default service
-export { service2,service3,service4,service5}
+export { service2,service3,service4,service5,service6}
