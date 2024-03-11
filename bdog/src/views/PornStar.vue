679db6c07bb07cd9bf0495f0b4aca520b4b398fa
tattoo1880 <tattoo186225345941@gmail.com>
2024-03-11 12:28:17 +0800

    overflow

--- bdog/src/views/PornStar.vue
+++ bdog/src/views/PornStar.vue
@@ -22,5 +22,11 @@ import SideBar from '../components/SideBar.vue'
 import PornStarC from '../components/PornStarC.vue'
 </script>
   
-<style scoped></style>
+<style scoped>
+.el-aside,
+.el-main {
+  height: calc(100vh - 60px); /* 60px 是 Header 的高度，根据实际情况调整 */
+  overflow: auto; /* 添加滚动条，当内容超出容器高度时 */
+}
+</style>
   
